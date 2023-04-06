24281aaa251c485bd643b94ee1df7f668cf15e62
marcusziade <marcus.ziade@ouraring.com>
2023-04-06 03:55:05 +0300

    Improve Apex Predator list UI

--- Legendstracker/Screens/ApexPredatorView.swift
+++ Legendstracker/Screens/ApexPredatorView.swift
@@ -11,44 +11,98 @@ struct ApexPredatorView: View {
         case .error(let errorMessage):
             Label(errorMessage, systemImage: "exclamationmark.triangle.fill")
         case .result(let predators):
-            List {
-                Section("Battle Royale") {
-                    VStack {
-                        Image(systemName: "desktopcomputer")
-                        Text(predators.pcRequiredRP)
-                        Text(predators.pcTotalMastersAndPredsBR)
+            NavigationView {
+                List {
+                    Section(header: Text("Battle Royale").bold().foregroundColor(.purple)) {
+                        HStack {
+                            Image(systemName: "desktopcomputer")
+                                .foregroundColor(.green)
+                            VStack(alignment: .leading) {
+                                Text("PC")
+                                    .font(.headline)
+                                Text("RP needed: \(predators.pcRequiredRP)")
+                                    .font(.subheadline)
+                                Text("Masters: \(predators.pcTotalMastersAndPredsBR)")
+                                    .font(.caption)
+                            }
+                        }
+                        HStack {
+                            Image(systemName: "logo.playstation")
+                                .foregroundColor(.blue)
+                            VStack(alignment: .leading) {
+                                Text("Playstation")
+                                    .font(.headline)
+                                Text("RP needed: \(predators.psRequiredRP)")
+                                    .font(.subheadline)
+                                Text("Masters: \(predators.psTotalMastersAndPredsBR)")
+                                    .font(.caption)
+                            }
+                        }
+                        HStack {
+                            Image(systemName: "logo.xbox")
+                                .foregroundColor(.green)
+                            VStack(alignment: .leading) {
+                                Text("Xbox")
+                                    .font(.headline)
+                                Text("RP needed: \(predators.x1RequiredRP)")
+                                    .font(.subheadline)
+                                Text("Masters: \(predators.x1TotalMastersAndPredsBR)")
+                                    .font(.caption)
+                            }
+                        }
                     }
-                    VStack {
-                        Image(systemName: "logo.playstation")
-                        Text(predators.psRequiredRP)
-                        Text(predators.psTotalMastersAndPredsBR)
+                    
+                    Section(header: Text("Arenas").bold().foregroundColor(.purple)) {
+                        HStack {
+                            Image(systemName: "desktopcomputer")
+                                .foregroundColor(.orange)
+                            VStack(alignment: .leading) {
+                                Text("PC")
+                                    .font(.headline)
+                                Text("AP needed: \(predators.pcRequiredAP)")
+                                    .font(.subheadline)
+                                Text("Masters: \(predators.pcTotalMastersAndPredsAP)")
+                                    .font(.caption)
+                            }
+                        }
+                        HStack {
+                            Image(systemName: "logo.playstation")
+                                .foregroundColor(.blue)
+                            VStack(alignment: .leading) {
+                                Text("Playstation")
+                                    .font(.headline)
+                                Text("AP needed: \(predators.psRequiredAP)")
+                                    .font(.subheadline)
+                                Text("Masters: \(predators.psTotalMastersAndPredsAP)")
+                                    .font(.caption)
+                            }
+                        }
+                        HStack {
+                            Image(systemName: "logo.xbox")
+                                .foregroundColor(.green)
+                            VStack(alignment: .leading) {
+                                Text("Xbox")
+                                    .font(.headline)
+                                Text("AP needed: \(predators.x1RequiredAP)")
+                                    .font(.subheadline)
+                                Text("Masters: \(predators.x1TotalMastersAndPredsAP)")
+                                    .font(.caption)
+                            }
+                        }
                     }
-                    VStack {
-                        Image(systemName: "logo.xbox")
-                        Text(predators.x1RequiredRP)
-                        Text(predators.x1TotalMastersAndPredsBR)
+                    
+                    Section {
+                        Text("To reach Apex Predator rank, players need to accumulate a certain amount of RP (Ranked Points) or AP (Arena Points) depending on the game mode. The number of Masters on each platform is also shown below.")
+                            .foregroundColor(.gray)
+                            .multilineTextAlignment(.leading)
+                        Text("Last updated: \(predators.timestampString)")
+                            .foregroundColor(.gray)
+                        WebView(url: URL(string: "https://www.youtube.com/embed/sTDqC7-GhCM?playsinline=1&mute=1&autoplay=1&controls=0&loop=1")!)
+                            .frame(height: 200)
+                            .cornerRadius(8)
                     }
                 }
-
-                Section("Arenas") {
-                    VStack {
-                        Image(systemName: "desktopcomputer")
-                        Text(predators.pcRequiredAP)
-                        Text(predators.pcTotalMastersAndPredsAP)
-                    }
-                    VStack {
-                        Image(systemName: "logo.playstation")
-                        Text(predators.psRequiredAP)
-                        Text(predators.psTotalMastersAndPredsAP)
-                    }
-                    VStack {
-                        Image(systemName: "logo.xbox")
-                        Text(predators.x1RequiredAP)
-                        Text(predators.x1TotalMastersAndPredsAP)
-                    }
-                }
-
-                Text(predators.timestampString)
+                .navigationTitle("Apex Predator Ranks")
             }
         case .empty:
             EmptyView()
@@ -59,5 +113,6 @@ struct ApexPredatorView: View {
 struct ApexPredatorView_Previews: PreviewProvider {
     static var previews: some View {
         ApexPredatorView(model: ApexPredatorVM.mock)
+            .preferredColorScheme(.dark)
     }
 }
---
+++ Legendstracker/Views/WebView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.allowsBackForwardNavigationGestures = false
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        uiView.load(request)
+    }
+}
